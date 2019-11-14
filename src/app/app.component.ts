eaf155784c77bbc3133de08ac07e342b977015e7
ron erlih <ronerlih@gmail.com>
2019-11-14 13:53:20 -0500

    universal addon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world';
+  tasks = ['click to get a random word'];
+  
+  clicked(){
+
+    fetch('http://api.wordnik.com/v4/words.json/randomWord?api_key=umi8964nkn3r28p7liaipy8k2ts3aov3fik9xtwy2rio7n6ha')
+      .then(data => data.json())
+      .then(data => {
+        console.log(data.word)
+        this.tasks = [data.word]})
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' })
   ],
   providers: [],
   bootstrap: [AppComponent]
